4750866afd6a7dc2bdaea1297007cb7c9a43e327
Rikam <rikampalkar@gmail.com>
2022-06-11 14:51:26 +0530

    The Stock Market App

---
+++ Stock Market App/StockMarketCharts/StockMarketCharts/Model/Stock.cs
@@ -0,0 +1,17 @@
+﻿namespace StockMarketCharts.Model
+{
+    public class Stock
+    {
+        public DateTime Date { get; set; }
+
+        public double Open { get; set; }
+
+        public double High { get; set; }
+
+        public double Low { get; set; }
+
+        public double Close { get; set; }
+
+        public long Volume { get; set; }
+    }
+}
